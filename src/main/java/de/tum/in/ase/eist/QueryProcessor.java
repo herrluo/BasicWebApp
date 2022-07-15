34a7fcb07eaafca47ee52ec26abf55abb1ca2565
ge37quf <ge37quf@mytum.de>
2022-07-15 13:29:17 +0200

    5 VERSUCH

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "SUPER_TUTORIUM";
+        } else if (query.contains("Joe Biden")) {
+            return "President of America";
         } else { // TODO extend the programm here
-            return "Hello World";
+            return "";
         }
     }
 }
